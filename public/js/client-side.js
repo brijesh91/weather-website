21b83462edadf765fccd29027eb6984d00df7fc7
brrijesh <brijesh.rathod@myglamm.com>
2020-05-07 19:01:18 +0530

    removed unnecessary console.log

--- public/js/client-side.js
+++ public/js/client-side.js
@@ -1,13 +1,8 @@
-console.log("Client side js script!")
-
 // fetch('http://puzzle.mead.io/puzzle').then((response) => {
 //     response.json().then((data) => {
 //         console.log(data)
 //     })
 // })
-
-
-
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
